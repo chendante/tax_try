1d3322c0e889f2290e445c2fc3c62e087c865812
chendante <danteliuzichen@163.com>
2020-12-11 16:52:18 +0800

    'fixbug'

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,5 @@
 from .CRIM.crim import Crim
 from .input_reader import InputReader
 from model.PBERT.sampler import Sampler
-from model.PAT.pat import Pat
 from model.trainer import SupervisedTrainer
 from model.PBERT import PBert
