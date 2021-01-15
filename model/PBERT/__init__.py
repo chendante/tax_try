e81f959f4de19deeeea4bf16f99c299c6cedcc35
chendante <danteliuzichen@163.com>
2021-01-15 14:36:30 +0800

    '1'

--- model/PBERT/__init__.py
+++
@@ -1,1 +0,0 @@
-from .pbert import PBert
--- run.py
+++ run.py
@@ -2,18 +2,18 @@ import argparse
 import args_config
 import model
 import configparser
+import util
 
 
 def _train():
-    args_parser = args_config.mine_args_parser()
-    args, _ = args_parser.parse_known_args()
-    args.taxo_path = "./data/raw_data/TExEval-2_testdata_1.2/gs_taxo/EN/science_wordnet_en.taxo"
-    args.epochs = 400
-    args.lr = 5e-5
-    args.eps = 1e-8
-    args.pretrained_path = "bert-base-uncased"
-    args.dic_path = "./data/preprocessed/science_dic.json"
-    args.padding_max = 256
+    args = dict(taxo_path="./data/raw_data/TExEval-2_testdata_1.2/gs_taxo/EN/science_wordnet_en.taxo",
+                epochs=30,
+                lr=3e-5,
+                eps=1e-8,
+                dic_path="./data/preprocessed/f_sci_wiki_dic.json",
+                padding_max=256,
+                pretrained_path="bert-base-uncased")
+    args = util.DotDict(args)
     trainer = model.SupervisedTrainer(args)
     trainer.train()
 
